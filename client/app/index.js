080382fb65f622a1e622b6783f37c5f0e023372a
Lovro <hodak.lovro@gmail.com>
2020-12-17 13:08:25 +0100

    front done

---
+++ client/app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div className='test'>Hello World</div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
